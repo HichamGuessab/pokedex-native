687930b5cab92a553b45ee9ff7d8281a68876641
Hicham Guessab <hichamgsb.pro@gmail.com>
2024-10-15 15:30:16 +0200

    Add a listing of PokemonCard

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,32 @@
-import {SafeAreaView, Text, StyleSheet} from "react-native";
-import {Link} from "expo-router";
+import {SafeAreaView, StyleSheet, View, Text, Image, FlatList} from "react-native";
 import React from "react";
 import {ThemedText} from "@/app/components/ThemedText";
 import {useThemeColors} from "@/app/hooks/useThemeColors";
 import {Card} from "@/app/components/Card";
+import {PokemonCard} from "@/app/components/pokemon/PokemonCard";
 
 export default function Index() {
     const colors = useThemeColors();
+    const pokemons = Array.from({length: 35}, (_, k) => ({
+        name: "Pokémon name",
+        id: k + 1
+    }))
   return (
     <SafeAreaView style={[styles.container, {backgroundColor: colors.tint}]}>
-        <Card>
-            <ThemedText variant="headline" color="grayDark">Pokédex</ThemedText>
+        <View style={styles.header}>
+            <Image source={require("@/assets/images/pokeball_white.png")} width={24} height={24}/>
+            <ThemedText variant="headline" color="grayLight">Pokédex</ThemedText>
+        </View>
+        <Card style={styles.body}>
+            <FlatList
+                data={pokemons}
+                numColumns={3}
+                contentContainerStyle={[styles.gridGap, styles.list]}
+                columnWrapperStyle={styles.gridGap}
+                renderItem={({item}) =>
+                    <PokemonCard id={item.id} name={item.name} style={{flex: 1/3}}/>
+                }
+                keyExtractor={(item) => item.id.toString()}/>
         </Card>
     </SafeAreaView>
   );
@@ -19,5 +35,20 @@ export default function Index() {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 16,
+        padding: 12,
+    },
+    body: {
+        flex: 1
+    },
+    gridGap: {
+        gap: 8
+    },
+    list: {
+        padding: 12,
     }
 })
